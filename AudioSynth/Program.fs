5ef4a468f89da0b1f4df67475805a7d0f2e709d3
Guillaume-Riviere <91249622+Guillaume-Riviere@users.noreply.github.com>
2022-01-05 19:26:32 +0100

    Made the calcSin function working
    Output a list of Coordinates for Sin graph

--- AudioSynth/Program.fs
+++ AudioSynth/Program.fs
@@ -10,18 +10,32 @@ open System
 let calcSin y m a = // Y = prescision, a = amplitude, m = period multiplier
     // a sin ( m x ) 
     let pointsList = new System.Collections.Generic.List<'T>()
-    let pointsNumbers = m / y
+    //let m = float(m)
+    let pointsNumbers = (m / y)
 
-    let y = (float y)
-    let m = (float m)
-    let a = (float a)
+    let pointsNumbers =  int (pointsNumbers)
+
+    let m = int(m)
+    // let y = (float y)
+    // let m = (float m)
+    // let a = (float a)
 
     
-    for i = 0 to pointsNumbers do
+    for i = 0 to ((314/2) * m) do
         let pointsNumbers = (float pointsNumbers)
+        let m = float(m)
         let i = (float i)
-        let xCoord = i * (pointsNumbers /(2./y))
-        let yCoord =  a * sin m * i
+
+        
+
+        let xCoord = (i * (2.*y)) /pointsNumbers
+
+        printfn "X : %O" xCoord
+
+        let yCoord =  a * sin (m * xCoord)
+
+        printfn "Y : %O" yCoord
+
         let point = (xCoord , yCoord)
         pointsList.Add(point) 
         pointsList
@@ -30,6 +44,6 @@ let calcSin y m a = // Y = prescision, a = amplitude, m = period multiplier
 
 [<EntryPoint>]
 let main argv =
-    let test = calcSin 1 2 2
+    let test = calcSin 0.1 2. 2.
     printfn "%A" test
     0 // return an integer exit code
