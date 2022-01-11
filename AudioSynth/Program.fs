d8b6137b38b8574fcc65f2243692025b3e37506e
Guillaume-Riviere <91249622+Guillaume-Riviere@users.noreply.github.com>
2022-01-11 14:08:42 +0100

    Changed CalcSin to fit Sound related parameters

--- AudioSynth/Program.fs
+++ AudioSynth/Program.fs
@@ -9,59 +9,54 @@ open System.IO
 
 
 // Define a function to construct a message to print
-let calcSin y m a xMultiplier = // Y = prescision, a = amplitude, m = period multiplier
-    let pointsNumbers = (m / y)
+// let calcSin y a xMultiplier = // Y = prescision, a = amplitude, m = period multiplier
+    // let pointsNumbers = (xMultiplier / y)
 
-    let pointsNumbers =  int (pointsNumbers)
+    // let pointsNumbers =  int (pointsNumbers)
+    // let m = int(xMultiplier)
+    // let xMultiplier = int (xMultiplier)
 
-    let m = int(m)
-    let xMultiplier = int (xMultiplier)
 
+
+// let sampleRate = 44100. 
+// let time = 2.
+// let freq = 12000.
+
+
+let calcSin sampleRate time freq =
+
+    let t = 1. + (1./sampleRate)
+    let N = sampleRate * time
+    let omega = 2. * System.Math.PI * freq
+
+
+    let points = [(0.)..t..N]
+    let points = points |> List.map(fun x -> sin(omega*x) )
+    // printfn "%O" testpoint.Length
+
+    points
+
+
+
+
+// // let calcSquare y m a = // Y = prescision, a = amplitude, m = period multiplier
+// //     // a sin ( m x ) 
+
+// //     let pointsList = calcSin y m a
+// //     for i in pointsList do
+// //         let y = snd(i)
+
+// //         let snd(i) = if y > 0 then a else -a
+// //         pointsList
+// //     pointsList
+// //         //printfn "%O" i
+
+// //     pointsList
     
-    let mutable pointsList = [
-        for j = 0 to xMultiplier do
-            for i = 0 to m do
-
-                let pointsNumbers = (float pointsNumbers)
-                let m = float(m)
-                let i = (float i)
-
-                let xCoord = ((i * (2.*y)) /pointsNumbers) + (float j * 3.14)
-
-                // printfn "X : %O" xCoord
-
-                // let yCoord =  a * sin (m * xCoord)
-                let yCoord = sin( 10.* (m * xCoord))
-                // printfn "Y : %O" yCoord
-
-                let yCoord = (yCoord + 1.)/2. * 255.
-                let byteY = byte(yCoord)
-                // printfn "%A" byteY
-
-                // printfn "%A" pointsList
-                yield yCoord
-    ]
-
-    pointsList
-
-// let calcSquare y m a = // Y = prescision, a = amplitude, m = period multiplier
-//     // a sin ( m x ) 
-
-//     let pointsList = calcSin y m a
-//     for i in pointsList do
-//         let y = snd(i)
-
-//         let snd(i) = if y > 0 then a else -a
-//         pointsList
-//     pointsList
-//         //printfn "%O" i
-
-//     pointsList
     
-
 [<EntryPoint>]
 let main argv =
-    let normalWave = calcSin 0.1 1. 8. 4410.
+    //let normalWave = calcSin 0.1 0.5 44.
 
     //let squareWave = calcSquare 0.1 2. 2.
     
@@ -79,18 +74,30 @@ let main argv =
         let headerSize = 16 in writer.Write(headerSize)
         let pcmFormat = 1s in writer.Write(pcmFormat)
         let mono = 1s in writer.Write(mono)
-        let sampleRate = 8000 in writer.Write(sampleRate)
+        let sampleRate = 44100 in writer.Write(sampleRate)
         let byteRate = sampleRate in writer.Write(byteRate)
         let blockAlign = 1s in writer.Write(blockAlign)
         let bitsPerSample = 8s in writer.Write(bitsPerSample)
+
+
+
+        // // fmt
+        // writer.Write("fmt "B)
+        // writer.Write(16) // Header size
+        // writer.Write(pcmFormat)
+        // writer.Write(nbChannels)
+        // writer.Write(sampleRate)
+        // writer.Write(byteRate)
+        // writer.Write(blockAlign)
+        // writer.Write(bitsPerSample)
         // data
         writer.Write("data"B)
         writer.Write(data.Length)
         writer.Write(data)
     let sample x = (x + 1.)/2. * 255. |> byte
-    let data = normalWave |> List.map (fun x -> byte x)
+    let data = calcSin 44100. 2. 440. |> List.map sample
     let data =  Microsoft.FSharp.Collections.List.toArray data
     // printfn "Data: %A" data
-    let stream = File.Create(@"test.Wav")
+    let stream = File.Create(@"test.wav")
     write stream data
     0 // return an integer exit code
