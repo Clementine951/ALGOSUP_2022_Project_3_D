32f868a93291d92d4915ad1b70528dbfcb355008
gael.lebrun <gael.lebrun@algosup.com>
2022-01-11 15:41:43 +0100

    Update Play.fs

--- AudioSynth/Play.fs
+++ AudioSynth/Play.fs
@@ -2,16 +2,16 @@ namespace AudioSynth
 
 
 open System
-open SFML.Audio
+open SFML.Audio //Allow to use SFML library
 open System.Threading
 
 module playSynth = 
-    let PlaySound (name:string) =
-        let buffer = new SoundBuffer(name)
-        let sound = new Sound(buffer)
-        sound.Play()
+    let PlaySound (name:string) = //Allow to choose your file
+        let buffer = new SoundBuffer(name) //Storage for audio sample defining a sound
+        let sound = new Sound(buffer) //Allow to use the sound stored in SoundBuffer
+        sound.Play() //Play the sound
 
         while sound.Status = SoundStatus.Playing do
             Thread.Sleep(100)
 
-    // let play = PlaySound "test.wav"
+    let play = PlaySound "test.wav" //Play the file you choose
