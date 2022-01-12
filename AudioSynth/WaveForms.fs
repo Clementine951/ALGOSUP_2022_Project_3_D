62b6b6401b5d607941f6f7ae5d1b8b9b6953603a
Victor-Leroy <victor.leroy@algosup.com>
2022-01-12 13:46:29 +0100

    Add Saw and Triangle wave

--- AudioSynth/WaveForms.fs
+++ AudioSynth/WaveForms.fs
@@ -14,3 +14,27 @@ module WaveGen =
         let points = points |> List.map(fun x -> amp * sin(omega*x) )
         // printfn "%O" testpoint.Length
         points
+
+
+
+    let calcTri sampleRate time freq amp =
+
+        let t = 1. + (1./sampleRate)
+        let N = sampleRate * time
+        let omega = 2. * System.Math.PI * freq
+
+        let points = [(0.)..t..N]
+        let points = points |> List.map(fun x ->  amp * 2. * asin (sin (2. * System.Math.PI * t * frequence)) / System.Math.PI )
+        // printfn "%O" testpoint.Length
+        points
+
+    let calcSaw sampleRate time freq amp =
+
+        let t = 1. + (1./sampleRate)
+        let N = sampleRate * time
+        let omega = 2. * System.Math.PI * freq
+
+        let points = [(0.)..t..N]
+        let points = points |> List.map(fun x ->  amp * 2. * (t * freq - floor (0.5 +  t * freq)) )
+        // printfn "%O" testpoint.Length
+        points
