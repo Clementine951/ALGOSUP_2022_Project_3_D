a849c1e6d1de7086c133cb8e7052953b2fc3744b
gael.lebrun <gael.lebrun@algosup.com>
2022-01-13 12:04:15 +0100

    Update Play.fs

--- AudioSynth/Play.fs
+++ AudioSynth/Play.fs
@@ -11,8 +11,8 @@ module playSynth =
     let PlaySound (name:string,save:bool,time:float32) = //Allow to choose your file and time of section do you want to read
         let buffer = new SoundBuffer(name) //Storage for audio sample defining a sound
         let sound = new Sound(buffer) //Allow to use the sound stored in SoundBuffer
-        let times = Time.FromSeconds(time) // cSet up time of file section
-        sound.set_PlayingOffset(times)// read the section of file 
+        let times = Time.FromSeconds(time) // Set up time of file section
+        sound.set_PlayingOffset(times)// Read the section of file 
         sound.Play() //Play the sound
 
         while sound.Status = SoundStatus.Playing do
