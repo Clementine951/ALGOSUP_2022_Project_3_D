870d851d8798a9e55a7940ea3a4feac3e66388f1
gael.lebrun <gael.lebrun@algosup.com>
2022-01-13 12:03:13 +0100

    Add comments

--- AudioSynth/Play.fs
+++ AudioSynth/Play.fs
@@ -11,7 +11,7 @@ module playSynth =
     let PlaySound (name:string,save:bool,time:float32) = //Allow to choose your file and time of section do you want to read
         let buffer = new SoundBuffer(name) //Storage for audio sample defining a sound
         let sound = new Sound(buffer) //Allow to use the sound stored in SoundBuffer
-        let times = Time.FromSeconds(time) // Set up time of file section
+        let times = Time.FromSeconds(time) // cSet up time of file section
         sound.set_PlayingOffset(times)// read the section of file 
         sound.Play() //Play the sound
 
@@ -19,6 +19,6 @@ module playSynth =
             Thread.Sleep(100)
 
 
-        if (save=false) then System.IO.File.Delete(name)
+        if (save=false) then System.IO.File.Delete(name) //Save, play and delete the sound
 
     //let play = PlaySound "test.wav" //Play the file you choose
