550097272d541655705fafa85857221580a3056a
Guillaume-Riviere <91249622+Guillaume-Riviere@users.noreply.github.com>
2022-01-18 16:37:12 +0100

    Add file save.fs
    Moved the function to save in a file to another file
    More accesible and more understandable

--- AudioSynth/Program.fs
+++ AudioSynth/Program.fs
@@ -34,34 +34,10 @@ module Main =
     //let normalWave = Filter.sinFlange 44100. 10. 40000.  1.
 
     /// Write WAVE PCM soundfile (8KHz Mono 8-bit)
-    let write stream (data:byte[]) =
-        use writer = new BinaryWriter(stream)
-        // RIFF
-        writer.Write("RIFF"B)
-        let size = 36 + data.Length in writer.Write(size)
-        writer.Write("WAVE"B)
-        // fmt
-        writer.Write("fmt "B)
-        let headerSize = 16 in writer.Write(headerSize)
-        let pcmFormat = 1s in writer.Write(pcmFormat)
-        let mono = 1s in writer.Write(mono)
-        let sampleRate = 44100 in writer.Write(sampleRate)
-        let byteRate = sampleRate in writer.Write(byteRate)
-        let blockAlign = 1s in writer.Write(blockAlign)
-        let bitsPerSample = 8s in writer.Write(bitsPerSample)
-
-        writer.Write("data"B)
-        writer.Write(data.Length)
-        writer.Write(data)
-    let sample x = (x + 1.)/2. * 255. |> byte
-    let data = normalWave 
-                |> List.map sample 
-                |> Microsoft.FSharp.Collections.List.toArray
-    // printfn "Data: %A" data
     let stream = File.Create(@"test.wav")
-    write stream data
+    save.write stream normalWave
 
 
 
-    PlaySynth.playSound ("test.wav" ,true ,float32(2.0))
+    PlaySynth.playSound ("test.wav" ,true ,float32(0.))
      // return an integer exit code
