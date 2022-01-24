d7f73da0bdf7073a7f206332b0c87aa009aa5cbf
Guillaume-Riviere <91249622+Guillaume-Riviere@users.noreply.github.com>
2022-01-24 12:14:00 +0100

    Update filter.fs - add working Echo
    Add echo function based on initial sound

--- AudioSynth/Filters.fs
+++ AudioSynth/Filters.fs
@@ -27,26 +27,20 @@ module Filter =
 
 
     let echo (initialList:list<float>) (delay:float) (amp:float) (repeat:float) =
-        //for i = 1 to repeat do
-        let delaySample = delay * repeat
-        let tmpList = WaveGen.calcSin 44100 delay 0. 0.
-        let baseList = initialList @ tmpList
-        let newList = amplitude (tmpList @ initialList) amp
-        // ADD FUNCTION TO REDUCE VOLUME / AMPLITUDE
-        let interate = newList.Length
-        printfn "initial %O" initialList.Length
-        printfn "tmp lenght %O" tmpList.Length
-        printfn "new list %O" newList.Length
-        printfn "Interate %O" interate
-        let returnList = [
-            for i = 0 to interate-1 do
-                // printfn "%O" i
-                if newList.Item(i) = 0. then yield baseList.Item(i)
-                elif i >= initialList.Length then yield newList.Item(i)
-                else 
-                    yield ((newList.Item(i) + baseList.Item(i))/2.)
+
+        let repeat = int repeat
+        let tmpList = WaveGen.calcSin 44100 delay 0. 0. // Define the silence time based on delay
+
+        let returnFullList = [
+            yield initialList // Start by returning the base sound
+            for i = 1 to repeat do  // Then return each echo effect
+                let i = float i
+
+                let returnList = tmpList @ (amplitude initialList ( amp / i)) // calling th amp function to reduce the sound each for each echo
+                yield returnList
         ]
-        returnList
+        let returnFullList = List.concat returnFullList // Fuse all list returned into only one
+        returnFullList
 
 
 
