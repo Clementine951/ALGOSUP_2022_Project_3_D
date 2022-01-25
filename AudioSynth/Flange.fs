ef972fd02d2c920b456c9747f08ced9ebf6d71d0
Victor-Leroy <victor.leroy@algosup.com>
2022-01-25 10:32:47 +0100

    Update Flange.fs
    Added commentaries

--- AudioSynth/Flange.fs
+++ AudioSynth/Flange.fs
@@ -10,8 +10,8 @@ open NoteToHz
 
 let flange (wave: float list)=
 
-        let lenght = wave.Length 
-        let subWave = [for i in 0..lenght-1 do wave.[i] * -1.]
+        let lenght = wave.Length // This function check the length of the wave
+        let subWave = [for i in 0..lenght-1 do wave.[i] * -1.] // This inverts all the values of the wave
         // let test =  for i in 0..lenght-1 do
         //             printfn"wave %A" wave.[i]
         //             printfn"subWave %A" subWave.[i]
