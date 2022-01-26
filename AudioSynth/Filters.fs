eda327f5eab7dddc6e329485a7d117b69359534b
Victor-Leroy <victor.leroy@algosup.com>
2022-01-26 11:17:51 +0100

    Added flange to filter
    Added flange to filters and deleted flange

--- AudioSynth/Filters.fs
+++ AudioSynth/Filters.fs
@@ -61,12 +61,13 @@ module Filters =
        let init =  Rev list
        let reverse = Reverse init
        reverse
-                
-       
-
-    // let flange wave (t:float, N:float)=
-
-    //     let subWave = [(0.)..t..N]
     
-    //     let subWave = subWave |> List.map(fun x -> wave * -1.)
-    //     subWave
+    // Create a flange effect
+    let flange (wave: float list)=
+
+        let lenght = wave.Length // This function check the length of the wave
+        let subWave = [for i in 0..lenght-1 do wave.[i] * -1.] // This inverts all the values of the wave
+        // let test =  for i in 0..lenght-1 do
+        //             printfn"wave %A" wave.[i]
+        //             printfn"subWave %A" subWave.[i]
+        subWave
